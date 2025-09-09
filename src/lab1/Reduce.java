fdec10d99c90c547e1a8cd8b75b260ea8dde880a
Kristin Han <kristin.han@mail.utoronto.ca>
2025-09-09 10:22:58 -0400

    add Reduce

---
+++ src/lab1/Reduce.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(reduce(100));
+    }
+
+    public static int reduce(int n) {
+        int cnt = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n/=2;
+            }
+            else {
+                n--;
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+}
